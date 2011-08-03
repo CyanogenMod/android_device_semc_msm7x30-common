63a23327dd32dd5edcb508d467c1efcc60debef2
Diogo Ferreira <deovferreira@gmail.com>
2011-08-03 18:41:17 +0100

    msm7x30: use combined sbin instead of the multistage stuff

--- custombootimg.mk
+++ custombootimg.mk
@@ -1,15 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(TARGET_PREBUILT_KERNEL) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/erase_image $(MKBOOTIMG)
+$(INSTALLED_BOOTIMAGE_TARGET): $(TARGET_PREBUILT_KERNEL) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/erase_image $(MKBOOTIMG) $(MINIGZIP) $(INTERNAL_BOOTIMAGE_FILES)
 	$(call pretty,"Boot image: $@")
-	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/stage2/
-	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/bin/
-	$(hide) cp $(recovery_ramdisk) $(PRODUCT_OUT)/combinedroot/stage2/recovery.cpio
-	$(hide) cp $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/combinedroot/stage2/boot.cpio
-	$(hide) cp $(LOCAL_PATH)/prebuilt/busybox $(PRODUCT_OUT)/combinedroot/bin/busybox
-	$(hide) cp $(LOCAL_PATH)/prebuilt/keycheck $(PRODUCT_OUT)/combinedroot/bin/keycheck
-	$(hide) cp $(LOCAL_PATH)/prebuilt/init $(PRODUCT_OUT)/combinedroot/init
+	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/
+	$(hide) cp -r $(PRODUCT_OUT)/root/* $(PRODUCT_OUT)/combinedroot/
+	$(hide) cp -r $(PRODUCT_OUT)/recovery/root/sbin/* $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/combinedroot/ > $(PRODUCT_OUT)/combinedroot.cpio
 	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | $(MINIGZIP) > $(PRODUCT_OUT)/combinedroot.fs
 	$(hide) $(MKBOOTIMG) --kernel $(TARGET_PREBUILT_KERNEL) --ramdisk $(PRODUCT_OUT)/combinedroot.fs --base $(BOARD_KERNEL_BASE) --output $@
+
+INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
+	$(recovery_ramdisk) \
+	$(recovery_kernel)
+	@echo ----- Making recovery image ------
+	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) --output $@
+	@echo ----- Made recovery image -------- $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+
