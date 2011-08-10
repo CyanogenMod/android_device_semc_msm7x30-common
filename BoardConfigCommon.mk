14ef5be07182222840d3fd3e3cc4fbf9b20c19fc
jerpelea <jerpelea@gmail.com>
2011-08-10 19:24:51 +0300

    semc:added CUSTOM_RECOVERY_KEYMAPPING for semc layout
    Change-Id: I3227eae5921879d20e23693e901fad56fb29e618

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,6 +53,7 @@ BOARD_HAS_SMALL_RECOVERY := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_USES_RECOVERY_CHARGEMODE := false
 BOARD_CUSTOM_GRAPHICS := ../../../device/semc/msm7x30-common/recovery/graphics.c
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../../device/semc/msm7x30-common/recovery/recovery_ui.c
 
 BOARD_KERNEL_CMDLINE := console=null
 BOARD_KERNEL_BASE := 0x00200000
