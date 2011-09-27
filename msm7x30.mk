0cb681d9fa010c0d1aefc0947426c7474c187c04
jerpelea <jerpelea@gmail.com>
2011-09-27 15:52:07 +0300

    semc: .sh and recovery update
    Change-Id: I26ae320911904a4133bf6d5417173e53e07b996c

--- msm7x30.mk
+++ msm7x30.mk
@@ -35,6 +35,22 @@ PRODUCT_COPY_FILES += \
     device/semc/msm7x30-common/prebuilt/10cpmodules:system/etc/init.d/10cpmodules \
     device/semc/msm7x30-common/prebuilt/dnsmasq.conf:system/etc/wifi/dnsmasq.conf
 
+#recovery resources
+PRODUCT_COPY_FILES += \
+    bootable/recovery/res/images/icon_firmware_error.png:root/res/images/icon_firmware_error.png \
+    bootable/recovery/res/images/icon_firmware_install.png:root/res/images/icon_firmware_install.png \
+    bootable/recovery/res/images/icon_clockwork.png:root/res/images/icon_clockwork.png \
+    bootable/recovery/res/images/icon_error.png:root/res/images/icon_error.png \
+    bootable/recovery/res/images/icon_installing.png:root/res/images/icon_installing.png \
+    bootable/recovery/res/images/indeterminate1.png:root/res/images/indeterminate1.png \
+    bootable/recovery/res/images/indeterminate2.png:root/res/images/indeterminate2.png \
+    bootable/recovery/res/images/indeterminate3.png:root/res/images/indeterminate3.png \
+    bootable/recovery/res/images/indeterminate4.png:root/res/images/indeterminate4.png \
+    bootable/recovery/res/images/indeterminate5.png:root/res/images/indeterminate5.png \
+    bootable/recovery/res/images/indeterminate6.png:root/res/images/indeterminate6.png \
+    bootable/recovery/res/images/progress_empty.png:root/res/images/progress_empty.png \
+    bootable/recovery/res/images/progress_fill.png:root/res/images/progress_fill.png
+
 PRODUCT_PACKAGES += \
 hostap\
     librs_jni \
