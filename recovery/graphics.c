da449de0541b86723c39b9f3db74fe4216d0fc3d
Diogo Ferreira <defer@cyanogenmod.com>
2011-11-25 16:17:19 +0000

    msm7x30-common: Update for ICS
    This is a combination of commits for intial ICS support by
    Diogo Ferreira <defer@cyanogenmod.com>
    jerpelea <jerpelea@gmail.com>
    
    - Added audio shim, camera shim.
    - Add include files
    - Fix recovery
    - Custom gralloc
    
    Change-Id: I032ff89fd3a9f0bf16b48920932c4df096625311
    
    msm7x30-common: Remove camera hal for now, as it's broken
    
    Change-Id: I5385854c5b4d2a12dc540713d2963813a7bd5aa9
    
    msm7x30: Update headers for ICS
    
    Change-Id: I96acebf66db80d6bb4595477d69964c1ac95b9eb
    
    msm7x30-common: Add private gralloc and fix header path
    
    Fixes the header path which was set to the previous device-specific
    path.
    
    Compiles the private version of gralloc.
    
    Change-Id: Idbe9deb5737f5a4fab9db8cfae7032c42d6964c1

--- recovery/graphics.c
+++ recovery/graphics.c
@@ -210,6 +210,13 @@ int gr_measure(const char *s)
     return gr_font->cwidth * strlen(s);
 }
 
+void gr_font_size(int *x, int *y)
+{
+    *x = gr_font->cwidth;
+    *y = gr_font->cheight;
+}
+
+
 int gr_text(int x, int y, const char *s)
 {
     GGLContext *gl = gr_context;
@@ -338,6 +345,9 @@ int gr_init(void)
     gl->enable(gl, GGL_BLEND);
     gl->blendFunc(gl, GGL_SRC_ALPHA, GGL_ONE_MINUS_SRC_ALPHA);
 
+    gr_fb_blank(true);
+    gr_fb_blank(false);
+
     return 0;
 }
 
@@ -366,4 +376,13 @@ int gr_fb_height(void)
 gr_pixel *gr_fb_data(void)
 {
     return (unsigned short *) gr_mem_surface.data;
+}
+
+void gr_fb_blank(bool blank)
+{
+    int ret;
+
+    ret = ioctl(gr_fb_fd, FBIOBLANK, blank ? FB_BLANK_POWERDOWN : FB_BLANK_UNBLANK);
+    if (ret < 0)
+        perror("ioctl(): blank");
 }
---
+++ recovery/recovery_keys.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <linux/input.h>
+
+#include "recovery_ui.h"
+#include "common.h"
+#include "extendedcommands.h"
+
+
+int device_toggle_display(volatile char* key_pressed, int key_code) {
+    // hold power and press volume-up
+    return key_pressed[KEY_POWER] && key_code == KEY_VOLUMEUP;
+}
+
+
+int device_handle_key(int key_code, int visible) {
+    if (visible) {
+        switch (key_code) {
+            case KEY_CAPSLOCK:
+            case KEY_DOWN:
+            case KEY_VOLUMEDOWN:
+            case KEY_MENU:
+                return HIGHLIGHT_DOWN;
+
+            case KEY_LEFTSHIFT:
+            case KEY_UP:
+            case KEY_VOLUMEUP:
+                return HIGHLIGHT_UP;
+
+            case KEY_POWER:
+                if (ui_get_showing_back_button()) {
+                    return SELECT_ITEM;
+                }
+                break;
+            case KEY_HOME:
+            case KEY_LEFTBRACE:
+            case KEY_ENTER:
+            case BTN_MOUSE:
+            case KEY_CAMERA:
+            case KEY_F21:
+            case KEY_SEND:
+                return SELECT_ITEM;
+            case KEY_END:
+            case KEY_BACKSPACE:
+            case KEY_SEARCH:
+                if (ui_get_showing_back_button()) {
+                    return SELECT_ITEM;
+                }
+            case KEY_BACK:
+                return GO_BACK;
+        }
+    }
+
+    return NO_ACTION;
+}
+
