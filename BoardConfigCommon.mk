d24367bf2813e102695ac414d6251ed7aba1d148
freexperia <jerpelea@gmail.com>
2012-01-23 17:18:56 +0200

    semc: updated with latest kernel and cleanup
    Change-Id: I7ca84f8d376616eefa5a3784036601e25a63ab47

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+TARGET_SPECIFIC_HEADER_PATH := device/semc/msm7x30-common/include
+
 TARGET_NO_BOOTLOADER := true
 
 TARGET_BOARD_PLATFORM := msm7x30
