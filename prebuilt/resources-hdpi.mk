3ccd28dd6b17fbaf5c45dd2143e5f7554848a5ee
Diogo Ferreira <defer@cyanogenmod.com>
2012-03-10 17:57:23 +0000

    msm7x30-common: Add makefiles for resolution-dependent resources
    These makefiles copy resolution-dependent resources to the target
    system. Right now they are used for the charging animations and have the
    purpose of avoiding duplication on each device tree.
    
    Change-Id: I30c7c63db160960ae2ff5bd2dacfa85210aead52

---
+++ prebuilt/resources-hdpi.mk
@@ -0,0 +1,9 @@
+#Offline charging animation
+PRODUCT_COPY_FILES += \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_01_H.png:system/semc/chargemon/data/charging_animation_01.png \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_02_H.png:system/semc/chargemon/data/charging_animation_02.png \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_03_H.png:system/semc/chargemon/data/charging_animation_03.png \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_04_H.png:system/semc/chargemon/data/charging_animation_04.png \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_05_H.png:system/semc/chargemon/data/charging_animation_05.png \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_06_H.png:system/semc/chargemon/data/charging_animation_06.png \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_07_H.png:system/semc/chargemon/data/charging_animation_07.png
---
+++ prebuilt/resources-mdpi.mk
@@ -0,0 +1,9 @@
+#Offline charging animation
+PRODUCT_COPY_FILES += \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_01_M.png:system/semc/chargemon/data/charging_animation_01.png \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_02_M.png:system/semc/chargemon/data/charging_animation_02.png \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_03_M.png:system/semc/chargemon/data/charging_animation_03.png \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_04_M.png:system/semc/chargemon/data/charging_animation_04.png \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_05_M.png:system/semc/chargemon/data/charging_animation_05.png \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_06_M.png:system/semc/chargemon/data/charging_animation_06.png \
+     device/semc/msm7x30-common/prebuilt/animations/charging_animation_07_M.png:system/semc/chargemon/data/charging_animation_07.png
