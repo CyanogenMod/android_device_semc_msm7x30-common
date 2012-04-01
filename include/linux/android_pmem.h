512f87460f3256723fb6c79c0321c693945c41bb
Diogo Ferreira <defer@cyanogenmod.com>
2012-04-01 19:32:19 +0100

    msm7x30-common: Align android_pmem.h with the new kernel version
    Change-Id: I16a30b0497f33f299d3d7df9699cf9c0e258fe18

--- include/linux/android_pmem.h
+++ include/linux/android_pmem.h
@@ -101,6 +101,7 @@ enum pmem_allocator_type {
 	 * defined
 	 */
 	PMEM_ALLOCATORTYPE_BITMAP = 0, /* forced to be zero here */
+	PMEM_ALLOCATORTYPE_SYSTEM,
 
 	PMEM_ALLOCATORTYPE_ALLORNOTHING,
 	PMEM_ALLOCATORTYPE_BUDDYBESTFIT,
@@ -155,6 +156,24 @@ struct android_pmem_platform_data
 	unsigned buffered;
 	/* This PMEM is on memory that may be powered off */
 	unsigned unstable;
+	/*
+	 * function to be called when the number of allocations goes from
+	 * 0 -> 1
+	 */
+	void (*request_region)(void *);
+	/*
+	 * function to be called when the number of allocations goes from
+	 * 1 -> 0
+	 */
+	void (*release_region)(void *);
+	/*
+	 * function to be called upon pmem registration
+	 */
+	void *(*setup_region)(void);
+	/*
+	 * indicates that this region should be mapped/unmaped as needed
+	 */
+	int map_on_demand;
 };
 
 int pmem_setup(struct android_pmem_platform_data *pdata,
@@ -166,4 +185,3 @@ int pmem_remap(struct pmem_region *region, struct file *file,
 #endif /* __KERNEL__ */
 
 #endif //_ANDROID_PPP_H_
-
