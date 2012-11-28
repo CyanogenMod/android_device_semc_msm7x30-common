52f1303f79e6f06df8e680b888651e268e8617e5
Alin Jerpelea <jerpelea@gmail.com>
2012-11-28 17:49:36 +0200

    msm7x30: compatibility with ics camera blob
    Change-Id: I2dcb68ff7c67ea81eb9cf4bf1922cded562f91a2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,13 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 USE_CAMERA_STUB := true
-BOARD_USES_LEGACY_CAMERA := true
-
 TARGET_SPECIFIC_HEADER_PATH := device/semc/msm7x30-common/include
 
 TARGET_NO_BOOTLOADER := true
-#BOARD_USES_UNCOMPRESSED_BOOT := true
 
+# Platform
 TARGET_BOARD_PLATFORM := msm7x30
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 TARGET_USES_2G_VM_SPLIT := true
@@ -28,22 +26,13 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
-#ARCH_ARM_HAVE_VFP := true
-#USE_MALLOC_ALIGNMENT := 16
 TARGET_HAVE_TSLIB := true
-
-#TARGET_EXTRA_CFLAGS += $(call cc-option,-march=armv7-a -mtune=cortex-a8)
 TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
-#TARGET_USE_SCORPION_PLD_SET := true
-#TARGET_SCORPION_BIONIC_PLDOFFS := 6
-#TARGET_SCORPION_BIONIC_PLDSIZE := 128
 
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
-BOARD_USES_GENERIC_AUDIO := false
-BOARD_HAVE_BLUETOOTH := true
-
+# QCOM Hardware
 BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QCOM_GPS := true
 BOARD_USES_QCOM_LIBS := true
@@ -52,19 +41,30 @@ USE_OPENGL_RENDERER := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_NO_HW_VSYNC := true
 COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DSEMC_RGBA_8888_OFFSET
-COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB -DQCOM_NO_SECURE_PLAYBACK -DANCIENT_GL
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DANCIENT_GL
 BOARD_EGL_CFG := device/semc/msm7x30-common/prebuilt/egl.cfg
+
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_HAVE_BLUETOOTH := true
 
 # Webkit
 TARGET_FORCE_CPU_UPLOAD := true
 ENABLE_WEBGL := true
 
+# Sensors
 SOMC_CFG_DASH_INCLUDED := yes
 
+# GPS
 BOARD_VENDOR_QCOM_AMSS_VERSION := 50000
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := semc
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
+# Camera
+BOARD_USES_LEGACY_CAMERA := true
+TARGET_DISABLE_ARM_PIE := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+
+# Recovery
 BOARD_CUSTOM_BOOTIMG_MK := device/semc/msm7x30-common/custombootimg.mk
 TARGET_RECOVERY_PRE_COMMAND := "touch /cache/recovery/boot;sync;"
 BOARD_HAS_SMALL_RECOVERY := true
@@ -79,11 +79,9 @@ BOARD_KERNEL_CMDLINE := console=null
 BOARD_KERNEL_BASE := 0x00200000
 BOARD_RECOVERY_BASE := 0x00200000
 
+# Boot Animation
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # A custom ota package maker for a device without an exposed boot partition
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/semc/msm7x30-common/releasetools/semc_ota_from_target_files
-
-# Light Sensor
-#BOARD_SYSFS_LIGHT_SENSOR := /sys/class/leds/lcd-backlight/als/enable
