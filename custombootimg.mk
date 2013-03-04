5e9c3906c49ab8390ef91fe9e5ebcdfae9aec0b5
ChampionSwimmer <android@championswimmer.tk>
2013-03-05 00:05:08 +0530

    increase pagesize to permanently remove the filler problem
    Ported from jellybean (was Change-Id: I493991aa2a5d95a180bb74d0264fa4a9cfdb4e2c)
    
    http://review.cyanogenmod.org/33214
    
    Signed-off-by: ChampionSwimmer <android@championswimmer.tk>
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I45700d30ab80976925edaf9d4514cb5d52742b83

--- custombootimg.mk
+++ custombootimg.mk
@@ -8,7 +8,7 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(recovery_ramdisk) $(INSTA
 	$(hide) cp -R $(PRODUCT_OUT)/recovery/root/sbin/* $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/combinedroot/ > $(PRODUCT_OUT)/combinedroot.cpio
 	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | gzip > $(PRODUCT_OUT)/combinedroot.fs
-	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel --ramdisk $(PRODUCT_OUT)/combinedroot.fs --base $(BOARD_KERNEL_BASE) --output $@
+	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel --ramdisk $(PRODUCT_OUT)/combinedroot.fs --base $(BOARD_KERNEL_BASE) --pagesize 131072 --output $@
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
