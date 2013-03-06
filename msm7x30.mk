9b8e51092a4199a27a729eae43435e8a1ed9d97d
Michael Bestas <mikeioannina@gmail.com>
2013-03-06 19:36:31 +0200

    msm7x30-common: remove fillers
    Not needed after the pagesize fix
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I4d4f18ad87d0f8fd35ac443cfcc7ee7d8456833b

--- msm7x30.mk
+++ msm7x30.mk
@@ -49,8 +49,6 @@ PRODUCT_COPY_FILES += \
     device/semc/msm7x30-common/prebuilt/fstab.semc:root/fstab.semc \
     device/semc/msm7x30-common/prebuilt/postrecoveryboot.sh:root/sbin/postrecoveryboot.sh
 
-#    device/semc/msm7x30-common/prebuilt/fillers:root/fillers \
-
 #Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
