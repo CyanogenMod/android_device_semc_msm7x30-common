ede3dffbbe78c9d6429bd48a96d24d2acb69f681
Alin Jerpelea <jerpelea@gmail.com>
2013-06-06 06:46:51 +0300

    msm7x30: fix recovery keys
    Change-Id: Ie8064717973a022638ecb3852c21809be21fd43b

--- recovery/recovery-keys.c
+++ recovery/recovery-keys.c
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <linux/input.h>
 
 #include "recovery_ui.h"
@@ -9,7 +25,6 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
     // hold power and press volume-up
     return key_pressed[KEY_POWER] && key_code == KEY_VOLUMEUP;
 }
-
 
 int device_handle_key(int key_code, int visible) {
     if (visible) {
@@ -29,6 +44,9 @@ int device_handle_key(int key_code, int visible) {
                 if (ui_get_showing_back_button()) {
                     return SELECT_ITEM;
                 }
+                if (!get_allow_toggle_display() && !ui_root_menu) {
+                    return GO_BACK;
+                }
                 break;
             case KEY_HOME:
             case KEY_LEFTBRACE:
@@ -44,8 +62,13 @@ int device_handle_key(int key_code, int visible) {
                 if (ui_get_showing_back_button()) {
                     return SELECT_ITEM;
                 }
+                if (!get_allow_toggle_display() && !ui_root_menu) {
+                    return GO_BACK;
+                }
             case KEY_BACK:
-                return GO_BACK;
+                if (!ui_root_menu) {
+                    return GO_BACK;
+                }
         }
     }
 
